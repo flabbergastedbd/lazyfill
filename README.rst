62588e92d42921bd884d9d223926f9148eb32472
tunnelshade <tunnelshade@gmail.com>
2018-04-20 16:25:07 +0530

    Added README.rst

---
+++ README.rst
@@ -0,0 +1,18 @@
+lazyfill
+########
+
+A small javascript library to make repetitive tasks like filling bug tracker details easier with some help
+from TamperMonkey.
+
+Installation
+~~~~~~~~~~~~
+
+1. Install TamperMonkey addon.
+2. Create a new tamper script from one of the `samples`_.
+3. Create templates as you need.
+
+For samples, have a look at `samples`_. Combining those samples gives you some powerful primitives. If you wish to see more
+functionality, have a loot at `lazyfill.api.js`_ to see what methods can be used.
+
+.. _samples: samples/
+.. _lazyfill.api.js: lazyfill.api.js
---
+++ samples/simplest.tamper.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Simplest fill
+// @namespace    https://tunnelshade.in/lazyfill/
+// @version      0.1
+// @description  try to take over the world!
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js#sha256=FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=
+// @require      https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.0/underscore-min.js#sha256=tr4FvXVZp8nkW7TvW4OYA5KWOs7fc2m5B6LN+AOn1VI=
+// @require https://cdn.rawgit.com/tunnelshade/lazyfill/cffcc5fc85d21b64be6ce42461795185ad194e73/lazyfill.api.js#sha256=wCctF+bhcQfMlcxUUc2hWQtxSP0IYWeFDGklR/kgyJk=
+// @match        https://jira.internal.site/
+// @grant        GM_registerMenuCommand
+// ==/UserScript==
+
+
+// This example walks you through a super simple flow where you want to fill only summary and description fields
+
+// Instantiate LazyFill object which will be used to store templates and do certain actions
+var lFill = new LazyFill();
+
+// Specify template fields and parameters, and write a custom fill function for necessary fields
+lFill.templates["Clickjacking"] = {
+    "url": /jira\.internal\.site/g,
+    "variables": {
+        "endpoints": "<TBF>"
+    },
+    "fields": [
+      {
+        "selector": "#title",
+        "content": "Missing X-Frame-Options or frame-ancestors on {endpoints}",
+      },
+      {
+        "selector": "#description",
+        "content": `
+h4. Issue
+
+Lack of X-Frame-Options header or CSP frame-ancestors headers on {endpoints} lead to clickjacking attacks. More help: https://www.owasp.org/index.php/Clickjacking
+
+h4. Fix
+
+Help: https://www.owasp.org/index.php/Clickjacking_Defense_Cheat_Sheet
+`
+      }
+    ]
+};
+
+// Start the lazyfill handlers
+lFill.start();
